129eea9fa205081305b93744a68304b0d0575a26
KaterynaGurina <katerynagurina@gmail.com>
2024-11-25 03:39:23 -0500

    Completed Philosopher and Monitor class (task 1 and 2), need to add explenation comments in philosopher

--- src/Monitor.java
+++ src/Monitor.java
@@ -11,14 +11,23 @@ public class Monitor
 	 * Data members
 	 * ------------
 	 */
-
+	private boolean[] chopsticks; // Tracks availability of chopsticks
+	private final int numPhilosophers;
+	private boolean isSomeoneTalking = false; // Tracks talking state
 
 	/**
 	 * Constructor
 	 */
-	public Monitor(int piNumberOfPhilosophers)
-	{
-		// TODO: set appropriate number of chopsticks based on the # of philosophers
+	// TODO: set appropriate number of chopsticks based on the # of philosophers
+
+	public Monitor(int numPhilosophers) {
+		this.numPhilosophers = numPhilosophers;
+		chopsticks = new boolean[numPhilosophers];
+
+		// Initialize all chopsticks to available
+		for (int i = 0; i < numPhilosophers; i++) {
+			chopsticks[i] = true; // true = available
+		}
 	}
 
 	/*
@@ -31,37 +40,97 @@ public class Monitor
 	 * Grants request (returns) to eat when both chopsticks/forks are available.
 	 * Else forces the philosopher to wait()
 	 */
-	public synchronized void pickUp(final int piTID)
-	{
-		// ...
+	//K: Allows a philosopher to pick up their left and right chopsticks to eat.
+	//If either chopstick is unavailable, the philosopher waits.
+	//Convert the philosopher's ID (piTID) into zero-based indices:
+	//Left chopstick: philosopherIndex (same as the philosopher’s index).
+	//Right chopstick: (philosopherIndex + 1) % numPhilosophers (circular wrap-around).
+	//Check if both chopsticks are available:
+	//If they are not, the philosopher calls wait() to release the lock and wait for a notification.
+	//When both chopsticks are free: Set their states to false (in use).
+	//The philosopher can proceed to eat.
+	public synchronized void pickUp(final int piTID) {
+		int philosopherIndex = piTID - 1; // Convert to zero-based index
+		int leftChopstick = philosopherIndex;
+		int rightChopstick = (philosopherIndex + 1) % numPhilosophers;
+
+		// Wait until both chopsticks are available
+		while (!chopsticks[leftChopstick] || !chopsticks[rightChopstick]) {
+			try {
+				wait(); // Wait for chopsticks to become available
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt(); // Restore interrupt status
+			}
+		}
+
+		// Pick up both chopsticks
+		chopsticks[leftChopstick] = false;
+		chopsticks[rightChopstick] = false;
 	}
 
 	/**
 	 * When a given philosopher's done eating, they put the chopstiks/forks down
 	 * and let others know they are available.
 	 */
-	public synchronized void putDown(final int piTID)
-	{
-		// ...
+	//K: Releases the left and right chopsticks after a philosopher finishes eating.
+	//Notifies all waiting philosophers that resources may now be available.
+	//Convert the philosopher's ID (piTID) into zero-based indices for their chopsticks.
+	//Set both chopsticks to true (available).
+	//Call notifyAll() to wake up all waiting philosophers so they can recheck their conditions.
+	public synchronized void putDown(final int piTID) {
+		int philosopherIndex = piTID - 1; // Convert to zero-based index
+		int leftChopstick = philosopherIndex;
+		int rightChopstick = (philosopherIndex + 1) % numPhilosophers;
+
+		// Put down both chopsticks
+		chopsticks[leftChopstick] = true;
+		chopsticks[rightChopstick] = true;
+
+		// Notify all waiting philosophers
+		notifyAll();
 	}
+
 
 	/**
 	 * Only one philopher at a time is allowed to philosophy
 	 * (while she is not eating).
 	 */
-	public synchronized void requestTalk()
-	{
-		// ...
+	//K: Ensures that only one philosopher can talk at a time.
+	//If another philosopher is talking, the current philosopher must wait.
+	//Use the isSomeoneTalking boolean to track if a philosopher is currently talking.
+	//If isSomeoneTalking is true, the philosopher calls wait() to release the lock and wait for a notification.
+	//Once the philosopher is allowed to talk, set isSomeoneTalking to true.
+	public synchronized void requestTalk() {
+		// Wait until no one is talking
+		while (isSomeoneTalking) {
+			try {
+				wait(); // Wait for a notification that talking is free
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt(); // Restore interrupt status
+			}
+		}
+
+		// Philosopher starts talking
+		isSomeoneTalking = true;
 	}
+
 
 	/**
 	 * When one philosopher is done talking stuff, others
 	 * can feel free to start talking.
 	 */
-	public synchronized void endTalk()
-	{
-		// ...
+	//K: Releases the talking privilege after a philosopher finishes talking.
+	//Notifies waiting philosophers that they can now talk.
+	//Set isSomeoneTalking to false (no one is talking).
+	//Call notifyAll() to wake up all waiting philosophers.
+	public synchronized void endTalk() {
+		// Philosopher finishes talking
+		isSomeoneTalking = false;
+
+		// Notify waiting philosophers
+		notifyAll();
 	}
+
 }
 
 // EOF
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,4 +1,8 @@
 import common.BaseThread;
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//Make sure you put comments for every task that involves coding to the changes
+//that you’ve made. This will be considered in the grading process.
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 /**
  * Class Philosopher.
@@ -6,119 +10,136 @@ import common.BaseThread;
  *
  * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
  */
-public class Philosopher extends BaseThread
-{
-	/**
-	 * Max time an action can take (in milliseconds)
-	 */
-	public static final long TIME_TO_WASTE = 1000;
+public class Philosopher extends BaseThread {
+    /**
+     * Max time any action can take (in milliseconds) (eating, thinking, or talking).
+     * Actions will take a random amount of time up to this value.
+     */
+    public static final long TIME_TO_WASTE = 1000;
 
-	/**
-	 * The act of eating.
-	 * - Print the fact that a given phil (their TID) has started eating.
-	 * - yield
-	 * - Then sleep() for a random interval.
-	 * - yield
-	 * - The print that they are done eating.
-	 */
-	public void eat()
-	{
-		try
-		{
-			// ...
-			sleep((long)(Math.random() * TIME_TO_WASTE));
-			// ...
-		}
-		catch(InterruptedException e)
-		{
-			System.err.println("Philosopher.eat():");
-			DiningPhilosophers.reportException(e);
-			System.exit(1);
-		}
-	}
+    /**
+     * The act of eating.
+     * - Print the fact that a given phil (their TID) has started eating.
+     * - yield
+     * - Then sleep() for a random interval.
+     * - yield
+     * - Then print that they are done eating.
+     */
+//K: I don't know what to put in the comment, the code is exactly what the comment above suggests line by line
+    public void eat() {
+        System.out.println("Philosopher " + getTID() + " starts eating.");
+        Thread.yield();
+        try {
+            sleep((long) (Math.random() * TIME_TO_WASTE)); // Random eating time
+        } catch (InterruptedException e) {
+            System.err.println("Philosopher.eat():");
+            DiningPhilosophers.reportException(e);
+            System.exit(1);
+        }
+        Thread.yield();
+        System.out.println("Philosopher " + getTID() + " finishes eating.");
+    }
 
-	/**
-	 * The act of thinking.
-	 * - Print the fact that a given phil (their TID) has started thinking.
-	 * - yield
-	 * - Then sleep() for a random interval.
-	 * - yield
-	 * - The print that they are done thinking.
-	 */
-	public void think()
-	{
-		// ...
-	}
 
-	/**
-	 * The act of talking.
-	 * - Print the fact that a given phil (their TID) has started talking.
-	 * - yield
-	 * - Say something brilliant at random
-	 * - yield
-	 * - The print that they are done talking.
-	 */
-	public void talk()
-	{
-		// ...
+    /**
+     * The act of thinking.
+     * - Print the fact that a given phil (their TID) has started thinking.
+     * - yield
+     * - Then sleep() for a random interval.
+     * - yield
+     * - The print that they are done thinking.
+     */
+    //K: same thing, just whats in the comment above
+    public void think() {
+        System.out.println("Philosopher " + getTID() + " starts thinking.");
+        Thread.yield();
+        try {
+            sleep((long) (Math.random() * TIME_TO_WASTE));
+                    }
+        catch (InterruptedException e) {
+            System.err.println("Philosopher.think():");
+            DiningPhilosophers.reportException(e);
+            System.exit(1);
+        }
+        Thread.yield();
+        System.out.println("Philosopher " + getTID() + " finishes thinking.");
 
-		saySomething();
+    }
 
-		// ...
-	}
+    /**
+     * The act of talking.
+     * - Print the fact that a given phil (their TID) has started talking.
+     * - yield
+     * - Say something brilliant at random
+     * - yield
+     * - The print that they are done talking.
+     */
+    public void talk() {
+        System.out.println("Philosopher " + getTID() + " starts talking.");
+        Thread.yield();
+        saySomething();
+        Thread.yield();
+        System.out.println("Philosopher " + getTID() + " finishes talk.");
+    }
 
-	/**
-	 * No, this is not the act of running, just the overridden Thread.run()
-	 */
-	public void run()
-	{
-		for(int i = 0; i < DiningPhilosophers.DINING_STEPS; i++)
-		{
-			DiningPhilosophers.soMonitor.pickUp(getTID());
+    /**
+     * No, this is not the act of running, just the overridden Thread.run()
+     */
+    //Comment
+    public void run() {
+        for (int i = 0; i < DiningPhilosophers.DINING_STEPS; i++) {
+            DiningPhilosophers.soMonitor.pickUp(getTID());
 
-			eat();
+            eat();
 
-			DiningPhilosophers.soMonitor.putDown(getTID());
+            DiningPhilosophers.soMonitor.putDown(getTID());
 
-			think();
+            think();
 
-			/*
-			 * TODO:
-			 * A decision is made at random whether this particular
-			 * philosopher is about to say something terribly useful.
-			 */
-			if(true == false)
-			{
-				// Some monitor ops down here...
-				talk();
-				// ...
-			}
+            /*
+             * TODO:
+             * A decision is made at random whether this particular
+             * philosopher is about to say something terribly useful.
+             */
+            if (Math.random() < 0.5) { // Em... what's with "true == false"?
+                DiningPhilosophers.soMonitor.requestTalk();
+                talk();
+                DiningPhilosophers.soMonitor.endTalk();
+            }
 
-			Thread.yield();
-		}
-	} // run()
+            Thread.yield();
+        }
+    } // run()
 
-	/**
-	 * Prints out a phrase from the array of phrases at random.
-	 * Feel free to add your own phrases.
-	 */
-	public void saySomething()
-	{
-		String[] astrPhrases =
-		{
-			"Eh, it's not easy to be a philosopher: eat, think, talk, eat...",
-			"You know, true is false and false is true if you think of it",
-			"2 + 2 = 5 for extremely large values of 2...",
-			"If thee cannot speak, thee must be silent",
-			"My number is " + getTID() + ""
-		};
+    /**
+     * Prints out a phrase from the array of phrases at random.
+     * Feel free to add your own phrases.
+     */
+    //K: I added more funny phrases
+    public void saySomething() {
+        String[] astrPhrases =
+                {
+                        "Eh, it's not easy to be a philosopher: eat, think, talk, eat...",
+                        "You know, true is false and false is true if you think of it",
+                        "2 + 2 = 5 for extremely large values of 2...",
+                        "If thee cannot speak, thee must be silent",
+                        "Why do we even eat? To distract ourselves from the meaning of existence.",
+                        "I think, therefore I am... hungry.",
+                        "One chopstick is lonely, two chopsticks make art.",
+                        "If a tree falls in the forest, can I eat it?",
+                        "Infinity is a long time, especially when you're waiting to eat.",
+                        "My number is " + getTID() + "",
+                        "Life is just a recursive loop: think, eat, repeat.",
+                        "Parallelism is great, except when someone else gets the chopsticks.",
+                        "They say sharing is caring, but not in this particular situation."
+                };
 
-		System.out.println
-		(
-			"Philosopher " + getTID() + " says: " +
-			astrPhrases[(int)(Math.random() * astrPhrases.length)]
-		);
-	}
+        System.out.println
+                (
+                        "Philosopher " + getTID() + " says: " +
+                                astrPhrases[(int) (Math.random() * astrPhrases.length)]
+                );
+    }
 }
 
 // EOF
